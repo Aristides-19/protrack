d27667f6d6b7b05685f1dea71378bb5f75977606
Arístides Pérez <aristp.2005@gmail.com>
2025-10-01 21:17:29 -0400

    feat: add button component and daisyui helper types

---
+++ src/components/input/button.tsx
@@ -0,0 +1,25 @@
+import { cn, type Size, type Variant } from "@lib";
+import type React from "react";
+
+/**
+ * @props { variant: btn-{variant}, size: btn-{size} }
+ * @link https://daisyui.com/components/button/
+ */
+const Button = ({ variant = "base", size = "md", ...props }: Props) => {
+  return (
+    <button
+      type="button"
+      {...props}
+      className={cn(`btn btn-${variant} btn-${size} btn-block px-8 shadow-none`, props.className)}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+type Props = React.ComponentPropsWithoutRef<"button"> & {
+  variant?: Variant | "ghost" | "link";
+  size?: Size;
+};
+
+export default Button;
--- src/lib/utils/style.ts
+++ src/lib/utils/style.ts
@@ -2,3 +2,15 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(...inputs));
+
+export type Size = "xs" | "sm" | "md" | "lg" | "xl";
+export type Variant =
+  | "base"
+  | "neutral"
+  | "primary"
+  | "secondary"
+  | "accent"
+  | "info"
+  | "success"
+  | "warning"
+  | "error";
