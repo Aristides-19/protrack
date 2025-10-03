ffc81fed760ae4bc08e5eb85885d4034bdea67dd
Arístides Pérez <aristp.2005@gmail.com>
2025-10-02 23:26:45 -0400

    feat: add @react-toastify for easy toasters

--- src/lib/context/AppContext.tsx
+++ src/lib/context/AppContext.tsx
@@ -1,6 +1,7 @@
 import type { Model } from "@lib";
 import { Supabase } from "@lib/config";
 import React from "react";
+import { toast } from "react-toastify";
 
 type AppContext = {
   user?: Pick<Model.User, "id"> & Partial<Pick<Model.User, "email">>;
@@ -10,14 +11,17 @@ const AppContext = React.createContext<AppContext>({});
 
 export const Provider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = React.useState<AppContext["user"]>();
+  const hasWelcomed = React.useRef(false);
 
   React.useEffect(() => {
     const {
       data: { subscription },
     } = Supabase.auth.onAuthStateChange((event, session) => {
-      if (["INITIAL_SESSION", "SIGNED_IN"].includes(event) && session?.user)
+      if (["INITIAL_SESSION", "SIGNED_IN"].includes(event) && session?.user) {
         setUser({ id: session.user.id, email: session.user.email });
-      if (event === "SIGNED_OUT") setUser(undefined);
+        if (!hasWelcomed.current) (toast.success("¡Bienvenido!"), (hasWelcomed.current = true));
+      }
+      if (event === "SIGNED_OUT") (setUser(undefined), toast.info("Has cerrado sesión"));
     });
 
     return () => subscription.unsubscribe();
--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -2,6 +2,8 @@ import { AppContext } from "@lib";
 import { Tanstack } from "@lib/config";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Outlet } from "react-router";
+import { Slide, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Layout = () => {
   return (
@@ -10,6 +12,20 @@ const Layout = () => {
         <main className="flex h-screen w-screen items-center justify-center bg-base-100">
           <Outlet />
         </main>
+        <ToastContainer
+          position="top-right"
+          autoClose={4000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={true}
+          closeButton={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+          transition={Slide}
+        />
       </AppContext.Provider>
     </QueryClientProvider>
   );
